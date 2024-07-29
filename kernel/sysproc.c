347a0833ee48cc91447dbc0041864a5d82db7f63
928166876 <79507477+928166876@users.noreply.github.com>
2024-07-29 13:58:55 +0800

    add syscall trace

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -95,3 +95,16 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+//kernel/sysproc.c
+uint64
+sys_trace(void)
+{
+  // int mask;
+  // if(argint(0, &mask) < 0)
+  //   return -1;
+  // myproc()->trace_mask = mask;
+
+  printf("sys_trace Hi\n");
+  return 0;
+}
